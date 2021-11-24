1d68abd0fb8ddaf53f14c9af7aa00fa8fbdde4cb
Moises Velasquez <mvelasquez@pictoric.com>
2021-11-23 19:29:57 -0500

    if conditional

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+const user = true;
+
+const textColor = {
+	primary: "text-primary",
+	warning: "text-warning",
+	danger: "text-danger",
+	success: "text-success",
+}
+
+const WelcomeGreeting = ()=>(
+	<h2 className={textColor.primary}>Welcome!</h2>
+)
+
+const FarewellGreeting = ()=>(
+	<h2 className={textColor.danger}>User offline</h2>
+)
+
 const App = () => {
 	return (
-		<div>
-			<h1>hello</h1>
+		<div className="container">
+			<h1 className={textColor.success}>hello</h1>
+			{ user ? <WelcomeGreeting/> : <FarewellGreeting/>}
 		</div>
 	)
 }
