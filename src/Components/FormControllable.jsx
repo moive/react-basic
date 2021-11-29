dc3524169d2adb0ff136c7a4cc645e11cea9b16d
Moises Velasquez <mvelasquez@pictoric.com>
2021-11-28 20:38:47 -0500

    refactoring code

--- src/Components/FormControllable.jsx
+++ src/Components/FormControllable.jsx
@@ -13,6 +13,13 @@ const FormControllable = () => {
         todoState:'pending'
     });
 
+    const handleChange = e=>{
+        setTodo({
+            ...todo,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <>
             <h2>Form</h2>
@@ -22,18 +29,18 @@ const FormControllable = () => {
                     name="todoName"
                     placeholder="Enter todo name"
                     className="form-control mb-2"
-                    onChange={e=> setTodo({...todo, todoName:e.target.value})}
+                    onChange={handleChange}
                 />
                 <textarea
                     name="todoDescription"
                     placeholder="Enter todo description"
                     className="form-control mb-2"
-                    onChange={e=> setTodo({...todo, todoDescription:e.target.value})}
+                    onChange={handleChange}
                 />
                 <select
                     name="todoState"
                     className="form-select mb-2"
-                    onChange={e=> setTodo({...todo, todoState: e.target.value})}
+                    onChange={handleChange}
                 >
                     <option value="pending">Pending</option>
                     <option value="completed">Completed</option>
