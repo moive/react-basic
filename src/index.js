55b108a44f77b5e4bcd3f7e79d7fe05c83578cd3
Moises Velasquez <mvelasquez@pictoric.com>
2021-12-10 17:07:49 -0500

    ordering rickandmorty

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import RickAndMorty from './Components/RickAndMorty';
+import RickAndMorty from './Components/RickMorty/RickAndMorty';
 
 ReactDOM.render(
   <React.StrictMode>
