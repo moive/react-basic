95fcd65fa19e85872901232aaf4c3a73177be65e
Moises Velasquez <mvelasquez@pictoric.com>
2021-12-12 20:41:24 -0500

    added home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-// import Counter from "./Components/Counter";
-// import FormNotControllable from "./Components/FormNotControllable";
-
-import TodoList from "./Components/TodoList";
-
-const user = true;
-
-const textColor = {
-	primary: "text-primary",
-	warning: "text-warning",
-	danger: "text-danger",
-	success: "text-success",
-}
-
-const WelcomeGreeting = ()=>(
-	<h2 className={textColor.primary}>Welcome!</h2>
-)
-
-const FarewellGreeting = ()=>(
-	<h2 className={textColor.danger}>User offline</h2>
-)
+import Home from "./Components/Home"
 
 const App = () => {
 	return (
 		<div className="container">
-			<h1 className={textColor.success}>hello</h1>
-			{ user ? <WelcomeGreeting/> : <FarewellGreeting/>}
-
-			{/* <Counter/> */}
-			{/* <FormNotControllable/> */}
-			<TodoList/>
+			<h1>app</h1>
+			{/* <Home/> */}
 		</div>
 	)
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+// import Counter from "./Components/Counter";
+// import FormNotControllable from "./Components/FormNotControllable";
+
+import TodoList from "./Components/TodoList";
+
+const user = true;
+
+const textColor = {
+	primary: "text-primary",
+	warning: "text-warning",
+	danger: "text-danger",
+	success: "text-success",
+}
+
+const WelcomeGreeting = ()=>(
+	<h2 className={textColor.primary}>Welcome!</h2>
+)
+
+const FarewellGreeting = ()=>(
+	<h2 className={textColor.danger}>User offline</h2>
+)
+
+const Home = () => {
+    return (
+        <>
+            <h1 className={textColor.success}>hello</h1>
+			{ user ? <WelcomeGreeting/> : <FarewellGreeting/>}
+
+			{/* <Counter/> */}
+			{/* <FormNotControllable/> */}
+			<TodoList/>
+        </>
+    )
+}
+
+export default Home
